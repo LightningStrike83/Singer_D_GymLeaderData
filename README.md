2ad42367edc6024e79b0f04b0f592a7d51e413c3
LightningStrike83 <113542893+LightningStrike83@users.noreply.github.com>
2023-08-08 22:10:39 -0400

    Files and Basic Code
    Added js files and started basic coding for data

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Singer_D_GymLeaderData
+
+Data for Red/Blue/Green/Yellow from: https://gamefaqs.gamespot.com/gameboy/367023-pokemon-red-version/faqs/64175/introduction
--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 console.log('Welcome to the world of Pokemon!')
+
+let rbgGame = document.querySelector ("#rbg");
+let brockRBG = document.querySelector ("#brock1");
+let mistyRBG = document.querySelector ("#misty1");
+
+function openRBG() {
+    let rbgChars = document.querySelector ("#rbg_chars");
+    if (rbgChars.style.display === "none") {
+        rbgChars.style.display = "flex";
+      } else {
+        rbgChars.style.display = "none";
+      }
+}
+
+function displayRBGTrainers() {
+    let rgbTrainer = this.dataset.member;
+
+    document.querySelector('#name1').textContent = rbg_trainers [rgbTrainer].name;
+    document.querySelector('#name2').textContent = rbg_trainers [rgbTrainer].name2;
+}
+
+rbgGame.addEventListener('click',openRBG);
+brockRBG.addEventListener('click', displayRBGTrainers)
+mistyRBG.addEventListener('click', displayRBGTrainers)
